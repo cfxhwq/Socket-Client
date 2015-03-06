b9fc7a7a00bdf6141a163de2ba471efea4993307
cfxhwq <xmhwq@sina.com>
2015-03-06 16:06:11 +0800

    complete

--- client.cpp
+++ client.cpp
@@ -26,23 +26,25 @@ using namespace std;
 
 #define MAXDATASIZE 100
 
-int main(int argc,char *argv[]) {
+int main(int argc, char *argv[]) {
 	int sockfd, num;
-	char buf [MẠ̣̣̣̣X̣DATASIZE];
+	char buf[MAXDATASIZE];
 	struct hostent *he;
 	struct sockaddr_in server;
 
-	if(argc != 2){
-		printf("ip%s\n",argv[0]);
+	if (argc != 2) {
+		printf("ip%s\n", argv[1]);
 		return 1;
 	}
 
-	if( (he = gethostbyname(argv[1])) == NULL){
-		printf("");
+	if ((he = gethostbyname(argv[1])) == NULL) {
+		printf("gethostbyname() error");
 		return 1;
+	} else {
+		printf("hostname:%s", he->h_name);
 	}
 
-	if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
 		perror("socket() error.");
 		return 1;
 	}
@@ -51,35 +53,22 @@ int main(int argc,char *argv[]) {
 
 	server.sin_family = AF_INET;
 	server.sin_port = htons(PORT);
-	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	server.sin_addr = *((struct in_addr *) he->h_addr);
+	cout << endl << server.sin_addr.s_addr << endl;
 
-	int opt = SO_REUSEADDR;
-	if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))
-			== -1) {
-		perror("setsockopt() error ");
+	if (connect(sockfd, (struct sockaddr *) &server, sizeof(server)) == -1) {
+		perror("connect() error.");
 		return 1;
 	}
 
-	if (bind(listenfd, (struct sockaddr *) &server, sizeof(server)) == -1) {
-		cout << server.sin_addr.s_addr << endl;
-		cout << listenfd << endl;
-		perror("bind() error");
+	if ((num = recv(sockfd, buf, MAXDATASIZE, 0)) == -1) {
+		perror("recv() error.");
 		return 1;
 	}
-	cout << "bind finish" << endl;
-	if (listen(listenfd, BACKLOG) == -1) {
-		perror("listen() error");
-		return 1;
-	}
-	cout << "listen finish" << endl;
-	addrlen = sizeof(client);
 
-	if ((connectfd = accept(listenfd, (struct sockaddr *) &client, &addrlen))) {
-		perror("accept() error\n");
-		return 1;
-	}
-	cout << "accept finish" << endl;
-	printf("Client's ip is %s,port is %d\n", inet_ntoa(client.sin_addr),
-			htons(client.sin_port));
+	buf[num-1] = '\0';
+	printf("server: %sa",buf);
+
+	close(sockfd);
 	return 0;
 }
